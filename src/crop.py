7c1a100d6c8e6df6575270810f466b03fe9f1be9
ftomei <ftomei@arpae.it>
2022-03-27 22:13:51 +0200

    optimal parameters

--- src/crop.py
+++ src/crop.py
@@ -34,10 +34,10 @@ class CCrop:
         self.laiMin = 1.0  # [m2 m-2]
         self.laiMax = 4.0  # [m2 m-2]
         self.rootDepthZero = 0.1  # [m]
-        self.rootDepthMax = 0.66  # [m]
+        self.rootDepthMax = 0.7  # [m]
         self.rootWidth = 2.1  # [m]
-        self.rootXDeformation = 0.46  # [-]
-        self.rootZDeformation = 0.48  # [-] 0: symmetric 1: cardioid 2: cardioid more accentuated
+        self.rootXDeformation = 0.48   # [-]
+        self.rootZDeformation = 0.5  # [-] 0:symmetric / 1:cardioid / 2:cardioid more accentuated
         self.kcMax = 2.5  # [-]
         self.fRAW = 0.6  # [-]
         self.setMaxValues()
