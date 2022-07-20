5098520db984144fb9b6198d689f4ced6d008d55
Joseph Giovanelli <j.giovanelli@unibo.it>
2022-07-20 18:14:07 +0200

    Add scripts

---
+++ scripts/run_tuning.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python src/tuning.py --path ./data/errano_tuning --num_iterations 5 --seed 42
---
+++ scripts/start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker stop watering_simulator
+docker rm watering_simulator
+docker build -t watering_simulator .
+docker run --name watering_simulator --volume $(pwd):/home --detach -t watering_simulator
+docker exec watering_simulator bash ./scripts/run_tuning.sh
