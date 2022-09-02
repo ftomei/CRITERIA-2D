4a535151852c310a34239d7ee2f702ec59f6bda9
ftomei <ftomei@arpae.it>
2022-09-02 19:28:59 +0200

    update

--- src/computeWaterContent/main.py
+++ src/computeWaterContent/main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def main():
     print(os.getcwd())
-    dataPath = os.path.join("data", "errano_2022")
+    dataPath = os.path.join("data", "errano_evaluation")
     settingsFolder = os.path.join(dataPath, "settings")
 
     print("Load soil...")
