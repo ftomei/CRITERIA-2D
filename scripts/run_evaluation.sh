7b9dfc5646f7cac7498d0a35a955e07d258699f1
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-01-25 10:37:22 +0100

    Update settings for evaluation experiments

--- scripts/run_evaluation.sh
+++ scripts/run_evaluation.sh
@@ -2,4 +2,4 @@
 cd src
 python cythonSetup.py build_ext --inplace
 cd ..
-python src/main.py --path ./data/errano_evaluation --iteration -1
+python src/main.py --path ./data/errano_evaluation_aug --iteration -1
