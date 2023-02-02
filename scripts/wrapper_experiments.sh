3c61bd979af3d85ed73e73609f23c18a5d9fa931
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-02-02 17:35:59 +0100

    Update wrapper_experiments.sh

--- scripts/wrapper_experiments.sh
+++ scripts/wrapper_experiments.sh
@@ -2,4 +2,4 @@
 cd src
 python cythonSetup.py build_ext --inplace
 cd ..
-python src/wrapper.py --path ./data/$1 --iteration $2 --seed 42
+python src/wrapper.py --path ./data/$1 --num_iterations $2 --seed 42
