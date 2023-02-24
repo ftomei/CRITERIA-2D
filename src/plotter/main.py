ee8f49b11dd77d93caf275632ebedc507b9a597a
Joseph Giovanelli <j.giovanelli@unibo.it>
2023-02-24 10:22:38 +0100

    Update charts

--- src/plotter/main.py
+++ src/plotter/main.py
@@ -5,12 +5,14 @@ from matplotlib.dates import DateFormatter
 import matplotlib.dates as mdates
 from sklearn.metrics import mean_squared_error
 import warnings
+
 warnings.filterwarnings("ignore")
 
 import os
 import time
 
 forbidden_sensors = ["z20_y0_x0", "z20_y0_x25", "z40_y0_x0", "z40_y0_x50"]
+
 
 def meteo(
     tuning_folder=os.path.join("data", "errano_tuning"),
@@ -52,7 +54,7 @@ def meteo(
     # add DataFrames to subplots
 
     for idx, meteo_var in reversed(list(enumerate(meteo_vars.keys()))):
-        df[meteo_var].plot(ax=axes[int(idx / nrows), idx % nrows], sharex=True)
+        df[meteo_var].plot(ax=axes[int(idx / nrows), idx % nrows], sharex=True, color='C7')
         # axes[int(idx / nrows), idx % nrows].set_xlim(
         #     [0, df.shape[0]]
         # )
@@ -134,10 +136,10 @@ def water(
     df = df.groupby("timestamp").sum()
 
     fig, ax = plt.subplots()
-    df.plot(ax=ax, kind="bar")
+    df.plot(ax=ax, kind="bar", color=['C9', 'C3'])
     ax.set_xlabel("")
     ax.set_xticks([0, 15, 29, 46, 60, df.shape[0] - 1])
-    ax.set_title("Precipitation and irrigation", fontdict={"fontsize": 18})
+    # ax.set_title("Precipitation and irrigation", fontdict={"fontsize": 18})
     ax2 = ax.twinx()
     ax.set_ylabel("L", rotation=0, labelpad=20, fontsize=15)
     ax2.set_ylabel("mm", rotation=0, labelpad=20, fontsize=15)
@@ -191,9 +193,9 @@ def ground_potential(
 
     for idx, meteo_var in reversed(list(enumerate(df.columns))):
         ax = axes[int(idx / ncols), idx % ncols]
-        df[meteo_var].plot(ax=ax, sharex=True)
+        df[meteo_var].plot(ax=ax, sharex=True, color= 'C5')
         ax.set_ylim([df.min().min(), -10])
-        ax.set_yscale('symlog')
+        ax.set_yscale("symlog")
         ax.set_xlabel("")
 
         # ax.set_xticks([0,  df.shape[0] - 1])
@@ -206,10 +208,10 @@ def ground_potential(
             ax.tick_params(length=0)
         ax.set_ylabel("cbar")
         ax.set_title(
-            meteo_var.replace("y0", "")
-            .replace("_", " ")
-            .replace("z", "depth = ")
-            .replace("x", "distance = ")
+            meteo_var.replace("y0_", "")
+            .replace("_", " cm, ")
+            .replace("z", "Depth = ")
+            .replace("x", "Distance = ") + " cm"
         )
 
     # _ = plt.xticks(rotation=0)
@@ -223,7 +225,7 @@ def forecast_avg(
     obs_folder=os.path.join("data", "errano_evaluation_1gg"),
     forecast_folder=os.path.join("data", "errano_evaluation"),
     output_folder=os.path.join("plots"),
-    with_forbidden_sensors=True
+    with_forbidden_sensors=True,
 ):
     support_dict = {
         "obs": os.path.join(obs_folder, "obs_data", "waterPotential.csv"),
@@ -270,8 +272,10 @@ def forecast_avg(
             # print(df[[c for c in df.columns if c.endswith(f"_{data_type}")]].iloc[:1])
             df[new_column] = [
                 mean_squared_error(
-                    df[[c for c in df.columns if c.endswith("_obs")]].iloc[i:(i+1)],
-                    df[[c for c in df.columns if c.endswith(f"_{data_type}")]].iloc[i:(i+1)],
+                    df[[c for c in df.columns if c.endswith("_obs")]].iloc[i : (i + 1)],
+                    df[[c for c in df.columns if c.endswith(f"_{data_type}")]].iloc[
+                        i : (i + 1)
+                    ],
                     squared=False,
                 )
                 for i in range(df.shape[0])
@@ -290,21 +294,28 @@ def forecast_avg(
     )
 
     df.plot(ax=ax)
-    ax.set_ylim([0, 2])
+    ax.set_ylim([0, 1.5])
     ax.set_xlabel("")
     ax.set_ylabel("logRMSE")
     fig.set_size_inches(13, 6)
     plt.tight_layout()
-    is_forbidden_sensors_string = "_with_forbidden_sensors" if with_forbidden_sensors else ""
-    fig.savefig(os.path.join(output_folder, f"forecasting_avg{is_forbidden_sensors_string}.pdf"))
-    fig.savefig(os.path.join(output_folder, f"forecasting_avg{is_forbidden_sensors_string}.png"))
+    is_forbidden_sensors_string = (
+        "_with_forbidden_sensors" if with_forbidden_sensors else ""
+    )
+    fig.savefig(
+        os.path.join(output_folder, f"forecasting_avg{is_forbidden_sensors_string}.pdf")
+    )
+    fig.savefig(
+        os.path.join(output_folder, f"forecasting_avg{is_forbidden_sensors_string}.png")
+    )
+    print(df)
 
 
 def forecast_sensor(
     obs_folder=os.path.join("data", "errano_evaluation_1gg"),
     forecast_folder=os.path.join("data", "errano_evaluation"),
     output_folder=os.path.join("plots"),
-    with_predicted=True
+    with_predicted=True,
 ):
     support_dict = {
         "obs": os.path.join(obs_folder, "obs_data", "waterPotential.csv"),
@@ -341,11 +352,15 @@ def forecast_sensor(
             f"_{data_type}"
         )
     for forecasting_day in ["1gg", "3gg", "7gg"]:
-        df = pd.concat([forecasting_dict["obs"], forecasting_dict[forecasting_day]], axis=1)
+        df = pd.concat(
+            [forecasting_dict["obs"], forecasting_dict[forecasting_day]], axis=1
+        )
         df = df.interpolate(method="linear", limit_direction="forward", axis=0)
         df = df.dropna(axis="index")
         df = df.reset_index()
-        sensor_columns = [c.replace("_obs", "") for c in df.columns if c.endswith("_obs")]
+        sensor_columns = [
+            c.replace("_obs", "") for c in df.columns if c.endswith("_obs")
+        ]
         if not with_predicted:
             new_columns = []
             for column in sensor_columns:
@@ -353,8 +368,8 @@ def forecast_sensor(
                 new_columns += [new_column]
                 df[new_column] = [
                     mean_squared_error(
-                        df[column + "_obs"].iloc[i:(i+1)],
-                        df[column + f"_{forecasting_day}"].iloc[i:(i+1)],
+                        df[column + "_obs"].iloc[i : (i + 1)],
+                        df[column + f"_{forecasting_day}"].iloc[i : (i + 1)],
                         squared=False,
                     )
                     for i in range(df.shape[0])
@@ -369,9 +384,15 @@ def forecast_sensor(
         nrows, ncols = 3, 4
         fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
 
-        for idx, meteo_var in reversed(list(enumerate(sensor_columns if with_predicted else df.columns))):
+        for idx, meteo_var in reversed(
+            list(enumerate(sensor_columns if with_predicted else df.columns))
+        ):
             ax = axes[int(idx / ncols), idx % ncols]
-            columns = [meteo_var + suffix for suffix in ["_obs", f"_{forecasting_day}"]] if with_predicted else meteo_var
+            columns = (
+                [meteo_var + suffix for suffix in ["_obs", f"_{forecasting_day}"]]
+                if with_predicted
+                else meteo_var
+            )
             df[columns].plot(ax=ax, sharex=True)
             ax.set_xlabel("")
             if not with_predicted:
@@ -379,7 +400,7 @@ def forecast_sensor(
                 ax.set_ylabel("logRMSE")
             else:
                 ax.set_ylim([df.min().min(), -10])
-                ax.set_yscale('symlog')
+                ax.set_yscale("symlog")
                 ax.set_ylabel("cbar")
 
             if idx < 8:
@@ -390,14 +411,159 @@ def forecast_sensor(
                 .replace("_", " ")
                 .replace("z", "depth = ")
                 .replace("x", "distance = "),
-                color="red" if meteo_var.replace("RMSE_", "") in forbidden_sensors else "black"
+                color="red"
+                if meteo_var.replace("RMSE_", "") in forbidden_sensors
+                else "black",
             )
 
         fig.set_size_inches(18, 6)
         plt.tight_layout()
         suffix = "_with_predicted" if with_predicted else ""
-        fig.savefig(os.path.join(output_folder, f"forecasting_sensor_{forecasting_day}{suffix}.pdf"))
-        fig.savefig(os.path.join(output_folder, f"forecasting_sensor_{forecasting_day}{suffix}.png"))
+        fig.savefig(
+            os.path.join(
+                output_folder, f"forecasting_sensor_{forecasting_day}{suffix}.pdf"
+            )
+        )
+        fig.savefig(
+            os.path.join(
+                output_folder, f"forecasting_sensor_{forecasting_day}{suffix}.png"
+            )
+        )
+
+
+def forecast_std(
+    rmse_df,
+    obs_folder=os.path.join("data", "errano_evaluation_1gg"),
+    output_folder=os.path.join("plots"),
+    with_forbidden_sensors=True,
+):
+    df = (
+        pd.DataFrame({"timestamp": [1655251200]})
+        .append(
+            pd.read_csv(os.path.join(obs_folder, "obs_data", "waterPotential.csv")),
+            ignore_index=True,
+        )
+        .set_index("timestamp")
+        .drop(columns=[] if with_forbidden_sensors else forbidden_sensors)
+    )
+    df = df.interpolate(method="linear", limit_direction="forward", axis=0)
+    df = df.dropna(axis="index")
+    df["average"] = df.mean(axis=1)
+    df = df.reset_index()
+    df = df.append(pd.DataFrame({"timestamp": [1655251200]}), ignore_index=True)
+    df = df.set_index("timestamp")
+    # df = df[new_columns]
+    df.index = pd.to_datetime(df.index, unit="s")
+
+    fig, ax = plt.subplots()
+
+    df["average"].plot(ax=ax)
+    ax.fill_between(df.index, df["average"] - (np.e ** rmse_df["forecasting horizon = 1gg"]), df["average"] + (np.e ** rmse_df["forecasting horizon = 1gg"]), alpha=0.35)
+    ax.set_xlabel("")
+    ax.set_ylabel("cbar")
+    fig.set_size_inches(13, 6)
+    plt.tight_layout()
+    is_forbidden_sensors_string = (
+        "_with_forbidden_sensors" if with_forbidden_sensors else ""
+    )
+    fig.savefig(
+        os.path.join(output_folder, f"forecasting_std{is_forbidden_sensors_string}.pdf")
+    )
+    fig.savefig(
+        os.path.join(output_folder, f"forecasting_std{is_forbidden_sensors_string}.png")
+    )
+
+
+def forecast_std2(
+    obs_folder=os.path.join("data", "errano_evaluation_1gg"),
+    forecast_folder=os.path.join("data", "errano_evaluation"),
+    output_folder=os.path.join("plots"),
+    with_forbidden_sensors=True,
+):
+    support_dict = {
+        "obs": os.path.join(obs_folder, "obs_data", "waterPotential.csv"),
+    }
+    for forecasting_day in ["1gg", "3gg", "7gg"]:
+        support_dict[forecasting_day] = os.path.join(
+            f"{forecast_folder}_{forecasting_day}", "output", "output.csv"
+        )
+
+    forecasting_dict = {}
+    for data_type, input_path in support_dict.items():
+        forecasting_dict[data_type] = (
+            pd.DataFrame({"timestamp": [1655251200]})
+            .append(
+                pd.read_csv(input_path),
+                ignore_index=True,
+            )
+            .set_index("timestamp")
+            .drop(columns=[] if with_forbidden_sensors else forbidden_sensors)
+        )
+        forecasting_dict[data_type][forecasting_dict[data_type] > -20] = -20
+        forecasting_dict[data_type] = forecasting_dict[data_type].reindex(
+            sorted(forecasting_dict[data_type].columns), axis=1
+        )
+        forecasting_dict[data_type] = forecasting_dict[data_type].add_suffix(
+            f"_{data_type}"
+        )
+    df = pd.concat(forecasting_dict.values(), axis=1)
+    df = df.interpolate(method="linear", limit_direction="forward", axis=0)
+    df = df.dropna(axis="index")
+    df = df.reset_index()
+    new_columns = []
+    for data_type in support_dict.keys():
+        if data_type != "obs":
+            new_column = f"RMSE_{data_type}"
+            new_columns += [new_column]
+            # print(df[[c for c in df.columns if c.endswith("_obs")]].iloc[0])
+            # print(df[[c for c in df.columns if c.endswith("_obs")]].iloc[:1])
+            # print(df[[c for c in df.columns if c.endswith(f"_{data_type}")]].iloc[0])
+            # print(df[[c for c in df.columns if c.endswith(f"_{data_type}")]].iloc[:1])
+            df[new_column] = [
+                mean_squared_error(
+                    df[[c for c in df.columns if c.endswith("_obs")]].iloc[i : (i + 1)],
+                    df[[c for c in df.columns if c.endswith(f"_{data_type}")]].iloc[
+                        i : (i + 1)
+                    ],
+                    squared=False,
+                )
+                for i in range(df.shape[0])
+            ]
+    df["average"] = df[[c for c in df.columns if c.endswith("_obs")]].mean(axis=1)
+    df = df.append(pd.DataFrame({"timestamp": [1655251200]}), ignore_index=True)
+    df = df.set_index("timestamp")
+    # df = df[new_columns]
+    df.index = pd.to_datetime(df.index, unit="s")
+
+    fig, ax = plt.subplots()
+    # df = df.rename(
+    #     columns={
+    #         column: column.replace("RMSE_", "forecasting horizon = ")
+    #         for column in df.columns
+    #     }
+    # )
+
+    df["average"].plot(ax=ax, color= 'C5', label="average")
+    # ax.set_ylim([-800, 0])
+    ax.set_yscale("symlog")
+    ax.fill_between(df.index, df["average"] - df["RMSE_7gg"], df["average"] + df["RMSE_7gg"], alpha=0.2, color='C2', label="RMSE on 7gg")
+    ax.fill_between(df.index, df["average"] - df["RMSE_3gg"], df["average"] + df["RMSE_3gg"], alpha=0.4, color='C1', label="RMSE on 3gg")
+    ax.fill_between(df.index, df["average"] - df["RMSE_1gg"], df["average"] + df["RMSE_1gg"], alpha=0.6, color='C0', label="RMSE on 1gg")
+    ax.legend()
+    ax.set_xlabel("")
+    ax.set_ylabel("cbar")
+    fig.set_size_inches(13, 6)
+    plt.tight_layout()
+    is_forbidden_sensors_string = (
+        "_with_forbidden_sensors" if with_forbidden_sensors else ""
+    )
+    fig.savefig(
+        os.path.join(output_folder, f"forecasting_std{is_forbidden_sensors_string}.pdf")
+    )
+    fig.savefig(
+        os.path.join(output_folder, f"forecasting_std{is_forbidden_sensors_string}.png")
+    )
+    return df
 
 
 def main():
@@ -405,9 +571,12 @@ def main():
     water()
     ground_potential()
     forecast_avg()
-    forecast_avg(with_forbidden_sensors=False)
-    forecast_sensor()
-    forecast_sensor(with_predicted=False)
+    # forecast_std(rmse_df)
+    # rmse_df = forecast_avg(with_forbidden_sensors=False)
+    # forecast_std(rmse_df, with_forbidden_sensors=False)
+    # forecast_sensor()
+    # forecast_sensor(with_predicted=False)
+    forecast_std2()
 
 
 if __name__ == "__main__":
